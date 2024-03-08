67c1985295b12d305feb0aa00ac344d7b80f9056
Mona Lisa <roldansebasdaza@gmail.com>
2024-03-08 10:48:19 -0500

    8/03/2024

--- UNA PAGINA/JS/no_abilitado.js
+++ UNA PAGINA/JS/no_abilitado.js
@@ -1,3 +1,11 @@
 document.getElementById("N").addEventListener("click", function() {
     alert("Este contenido no esta disponible lamentamos las molestias.");
-});
+})
+
+document.getElementById("N2").addEventListener("click", function() {
+    alert("Este contenido no esta disponible lamentamos las molestias.");
+})
+
+document.getElementById("N3").addEventListener("click", function() {
+    alert("INFORMACION ENVIADA CORRECTAMENTE :D");
+})
